26cfc081da63ef97ca87d94f029c72e776c888bd
chroe05 <chorevmichal@gmail.com>
2023-04-14 14:55:54 +0100

    added for lambda function to generate presigned url

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -5,15 +5,20 @@ import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
 import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
-    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+    // Return a presigned URL to upload a file for a TODO item with the provided id
+    const uploadUrl = await createAttachmentPresignedUrl(todoId);
 
-    return undefined
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        'uploadUrl': uploadUrl
+      })
+    }
   }
 )
 
@@ -21,6 +26,8 @@ handler
   .use(httpErrorHandler())
   .use(
     cors({
-      credentials: true
+      credentials: true,
+      orign: '*',
+      headers: true
     })
   )
