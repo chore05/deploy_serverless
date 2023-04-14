c25cb6469374d613d3ac6c1da08f04aaa1940b75
chroe05 <chorevmichal@gmail.com>
2023-04-14 12:41:07 +0100

    seperated code to datalayer and business login. Implementd signed url generation

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -2,5 +2,23 @@ import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 
 const XAWS = AWSXRay.captureAWS(AWS)
+const s3 = new XAWS.S3({
+    signatureVersion: 'v4'
+})
 
-// TODO: Implement the fileStogare logic
+// Implementation of fileStogare logic
+export class AttachmentUtils{
+    constructor(
+        private readonly s3BucketName:string = process.env.ATTACHMENT_S3_BUCKET
+    ){}
+    async createUploadUrl(todoId:string){
+        const expireDate = process.env.SIGNED_URL_EXPIRATION;
+        const uploadUrl:string = await s3.getSignedUrl(
+            'putObject', {
+                Bucket: this.s3BucketName,
+                Key: `${todoId}-img`,
+                Expires: expireDate
+        })
+        return uploadUrl
+    }
+}
